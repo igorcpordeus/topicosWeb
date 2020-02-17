b9fd16b6dac7475b2bdb12e83ce1a7aa85f9b7c7
Glaydson Vasconcelos de Sousa <glaydson.sousa@bcb.gov.br>
2020-02-17 17:02:35 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'gp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agp2020-s1';
+  titulo = 'Angular: Iniciando';
 }
---
+++ src/app/home/bem-vindo.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './bem-vindo.component.html'
+})
+export class BemVindoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
